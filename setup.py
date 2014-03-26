d2e4820e08c67586ccbb826497c018f3cc56d8f9
Ben Willmore <bdeb@willmore.eu>
2014-03-26 18:57:26 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+setup(name='kwiklib',
+      version='1.0',
+      packages=['kwiklib', 'kwiklib/dataio', 'kwiklib/utils'],
+      scripts=['kwiklib/scripts/runkwikkonvert.py'],
+      )
